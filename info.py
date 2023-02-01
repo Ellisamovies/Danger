96da3ca2826b215b75588c4869fd5015f0f5a9bf
Ellisamovies <121487082+Ellisamovies@users.noreply.github.com>
2023-02-01 15:50:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://ellisamovies:<7739ellisa>@cluster0.xbolpng.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://ellisa1234:<ellisa12345>@cluster0.8oggb8x.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Danger123")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
